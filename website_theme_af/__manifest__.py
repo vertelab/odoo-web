08f5e80bdad77a2050029d1c100cc8f5d5034b93
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2020-11-23 18:43:06 +0530

    [ADD] website_theme_af: AFC-613 Added files in website_theme_af.

--- website_theme_af/__manifest__.py
+++ website_theme_af/__manifest__.py
@@ -1,10 +1,11 @@
 {
     "name": "AF Website Tema V12",
     "summary": "AF Website Theme V12	",
-    "version": "12.0.0.2",
+    "version": "12.0.0.3",
     "category": "Theme/Website",
     "description": """
 		AF Website tema för Odoo 12.0 community edition.
+		12.0.0.3 -  This update adds changes to the main navigation to enable screen readers (A11y-update).
     """,
     "installable": True,
     "depends": [
@@ -13,5 +14,8 @@
     "data": [
         'views/assets.xml',
         'views/template.xml'
+    ],
+    'qweb': [
+        "static/src/xml/menu.xml",
     ]
 }
