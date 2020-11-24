a9ee7730c3949510b6021cdff53211c60be0ce9f
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2020-11-24 15:08:05 +0530

    [ADD] web_backend_theme_af, web_a11y_filter_view: AFC-1445 Added model name in Create and Edit button.

---
+++ web_a11y_filter_view/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Web A11y Filter View",
+    "summary": "Web Ally Filter View",
+    "version": "12.0.1.0",
+    "category": "Theme/Backend",
+    "description": """
+		Added model description on Create and Edit button.
+    """,
+    "author": "Vertel AB",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        'web'
+    ],
+    "qweb": [
+        "static/src/xml/base.xml",
+    ],
+    "data": [
+    ]
+}
