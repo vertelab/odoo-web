918cf8fcc8192df5c48c34d030a543df3b02223f
Fredrik Arvas <fredrik@arvas.se>
2020-12-10 06:24:01 +0100

    fixed typos in manifest

--- web_dashboard_dafa/__manifest__.py
+++ web_dashboard_dafa/__manifest__.py
@@ -1,11 +1,12 @@
 {
-    "name": " Web Dashboard Data",
-    "version": "12.0.0.1",
+    "name": " Web Dashboard Dafa",
+    "version": "12.0.0.2",
     "description": """
      This module shows the dashbord of an employee´s daily work. \n
-     12.0.0.1 - This update Adds an instruction page
+     12.0.0.1 - This update Adds an instruction page\n
+     12.0.0.2 - Corrected typo in module-name\n
 """,
-    "summary": "This module shows the dashbord of an employee´s daily work.",
+    "summary": "This module shows the dashbord of a users daily work.",
     "author": "Vertel AB",
     "license": "AGPL-3",
     "website": "https://vertel.se/",
@@ -20,6 +21,6 @@
         "security/ir.model.access.csv",
     ],
     'post_init_hook': 'post_init_hook',
-    "application": False,
-    "installable": True,
+    "application": "False",
+    "installable": "True",
 }
