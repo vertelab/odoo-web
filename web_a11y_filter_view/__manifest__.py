8c1942a5c9572dead9e1eb2579e1893dced56746
Fredrik Arvas <fredrik@arvas.se>
2020-12-13 09:57:10 +0100

    Removed due to error

--- web_a11y_filter_view/__manifest__.py
+++ web_a11y_filter_view/__manifest__.py
@@ -1,14 +1,15 @@
 {
     "name": "Web A11y Filter View",
     "summary": "Web Ally Filter View",
-    "version": "12.0.1.0",
+    "version": "12.0.1.1",
     "category": "Theme/Backend",
     "description": """
 		Added model description on Create and Edit button.
+		Removed due to error
     """,
     "author": "Vertel AB",
     "license": "LGPL-3",
-    "installable": True,
+    "installable": False,
     "depends": [
         'web'
     ],
