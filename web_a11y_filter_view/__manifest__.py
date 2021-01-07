c58ef0997906a45774f166916a1e12abef1ed159
Fredrik Arvas <fredrik@arvas.se>
2021-01-07 11:35:03 +0100

    Replaced by web_a11y_create-buttons

--- web_a11y_filter_view/__manifest__.py
+++ web_a11y_filter_view/__manifest__.py
@@ -1,11 +1,11 @@
 {
-    "name": "Web A11y Filter View",
+    "name": "Replaced - Web A11y Filter View",
     "summary": "Web Ally Filter View",
-    "version": "12.0.1.2",
+    "version": "12.0.1.3",
     "category": "Theme/Backend",
     "description": """
 		Added model description on Create and Edit button.
-		Removed due to error
+		Replaced by web_a11y_create-buttons
     """,
     "author": "Vertel AB",
     "license": "LGPL-3",
