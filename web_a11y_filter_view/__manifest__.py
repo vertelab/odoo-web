29d9d4eaf61cbdaf425e19a38ef059e120642061
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-01-29 16:05:29 +0100

    replaced by web_a11y_create-buttons

--- web_a11y_filter_view/__manifest__.py
+++ web_a11y_filter_view/__manifest__.py
@@ -1,11 +1,12 @@
 {
-    "name": "Web A11y Filter View",
+    "name": "Replaced by web_a11y_create-buttons ",
     "summary": "Web Ally Filter View",
     "version": "12.0.1.1",
     "category": "Theme/Backend",
     "description": """
 		Added model description on Create and Edit button.
 		Removed due to error
+		Replaced by odoo-web/web_a11y_create-buttons
     """,
     "author": "Vertel AB",
     "license": "LGPL-3",
