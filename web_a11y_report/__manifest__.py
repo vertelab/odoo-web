282406968d4931741f2905b1c392b9d018f066b8
Fredrik Arvas <fredrik@arvas.se>
2021-02-19 10:57:08 +0100

    Update __manifest__.py

--- web_a11y_report/__manifest__.py
+++ web_a11y_report/__manifest__.py
@@ -1,11 +1,12 @@
 {
     "name": "Accessibility Report",
     "summary": "Accessibility Report",
-    "version": "12.0.3",
+    "version": "12.0.1.0.4",
     "description": """
     	This module adds a section in the global settings for Accessibility Report to display a report of the 
     	backoffice adherance to the Accessibility rules.\n
-        v12.0.3 continued testing. AFC-1326
+        v12.0.3 continued testing. AFC-1326\n
+        v12.0.1..4 - added new content.
     """,
     "author": "Vertel AB",
     "license": "LGPL-3",
