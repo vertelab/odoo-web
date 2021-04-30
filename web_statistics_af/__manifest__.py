96b6b2cd1edfa5ff36fc3f699c1ddf53666664e7
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-04-30 10:45:24 +0530

    [ADD] web_statistics_af: AFC-1822 Updated description in manifest.

--- web_statistics_af/__manifest__.py
+++ web_statistics_af/__manifest__.py
@@ -7,11 +7,13 @@
     "version": "12.0.1.0.3",
     "category": "TBD",
     "description": """AF Statistics for Matomo
-    
-// CRM PROD https://af.analytics.elx.cloud/js/container_4v00kfpD.js
-// Test CRM: https://af-staging.analytics.elx.cloud/js/container_ew8X7e8B.js
-// DAFA PROD https://af.analytics.elx.cloud/js/container _t72Scj0c.js
-// Test DAFA: https://af-staging.analytics.elx.cloud/js/container_n7teu7kr.js
+    After module installation Go to Setting >> Technical >> Parameters >> System Parameters. \n
+    Search 'web_statistics_af.matomo_tag_src' key parameter. \n
+    Update this parameter value with one the following link: \n 
+// CRM PROD https://af.analytics.elx.cloud/js/container_4v00kfpD.js \n
+// Test CRM: https://af-staging.analytics.elx.cloud/js/container_ew8X7e8B.js \n
+// DAFA PROD https://af.analytics.elx.cloud/js/container _t72Scj0c.js \n
+// Test DAFA: https://af-staging.analytics.elx.cloud/js/container_n7teu7kr.js \n
 
 
 """,
