38eccf5aedfb62073053db634e4dca15115d97a4
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-04-30 10:27:57 +0530

    [ADD] web_statistics_af: AFC-1822 Added web_statistics_af module and taken matomo tag source from system parameter.

---
+++ web_statistics_af/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "AF Statistics",
+    "summary": "AF Statistics for Matomo",
+    "version": "12.0.1.0.3",
+    "category": "TBD",
+    "description": """AF Statistics for Matomo
+    
+// CRM PROD https://af.analytics.elx.cloud/js/container_4v00kfpD.js
+// Test CRM: https://af-staging.analytics.elx.cloud/js/container_ew8X7e8B.js
+// DAFA PROD https://af.analytics.elx.cloud/js/container _t72Scj0c.js
+// Test DAFA: https://af-staging.analytics.elx.cloud/js/container_n7teu7kr.js
+
+
+""",
+    "author": "AF",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["web"],
+    "data": [ "views/assets.xml",
+              "data/ir.config_parameter.csv"]
+}
