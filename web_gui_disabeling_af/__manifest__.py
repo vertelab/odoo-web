280026599a60ef66fe2b0074e252f6a24e4bbcdc
Robin Chatfield <robin.chatfield@arbetsformedlingen.se>
2021-05-12 13:54:12 +0200

    AFC-2229 web_gui_disabeling_af: Updated manifest.

--- web_gui_disabeling_af/__manifest__.py
+++ web_gui_disabeling_af/__manifest__.py
@@ -1,12 +1,13 @@
 {
     "name": "Web GUI Disabeling AF",
-    "version": "12.0.0.0",
+    "version": "12.0.1.0.0",
     "author": "Vertel AB",
     "license": "AGPL-3",
     "website": "https://vertel.se/",
     "category": "Tools",
 	"description": """
 	 v12.0.0.0 AFC-1225 Hide settings for the logged in user
+	 v12.0.1.0.0 AFC-2229: Removed user menu (top right).
     """,
     "depends": ["base"],
     "external_dependencies": [],
