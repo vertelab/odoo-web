5b624aac68f231dcc52b140cf22a9a402f281256
Miracle Ayodele <miracleaayodele@gmail.com>
2021-08-05 13:23:27 +0100

    added new form view

---
+++ web_form_search/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Web Form Search",
+    'summary': "Web Form Search",
+    'description': """Search Record from the Form View""",
+    'version': '12.0.1',
+    'depends': ['web', 'contacts', 'base_geolocalize'],
+    'data': [
+        "views/assets.xml",
+        "views/data.xml",
+        "views/res_partner.xml"
+    ],
+    'qweb': [],
+    'installable': True,
+    'auto_install': True
+}
