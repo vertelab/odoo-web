9ae428ab4298b1f31ee81a813af6cf44cd50ba8b
Miracle Ayodele <miracleaayodele@gmail.com>
2021-08-08 19:23:37 +0100

    web search form view

--- web_form_search/__manifest__.py
+++ web_form_search/__manifest__.py
@@ -4,10 +4,9 @@
     'summary': "Web Form Search",
     'description': """Search Record from the Form View""",
     'version': '12.0.1',
-    'depends': ['web', 'contacts', 'base_geolocalize'],
+    'depends': ['web', 'contacts'],
     'data': [
         "views/assets.xml",
-        "views/data.xml",
         "views/res_partner.xml"
     ],
     'qweb': [],
