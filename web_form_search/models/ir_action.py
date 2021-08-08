e0294d2ecbddd2459171a9b84134ef5795739b5b
Miracle Ayodele <miracleaayodele@gmail.com>
2021-08-08 19:23:37 +0100

    web search form view

--- web_form_search/models/ir_action.py
+++ web_form_search/models/ir_action.py
@@ -5,6 +5,8 @@ from odoo import fields, models
 class ActWindowView(models.Model):
     _inherit = 'ir.actions.act_window.view'
 
-    view_mode = fields.Selection(selection_add=[('form_search', "Form Search")])
+    view_mode = fields.Selection(selection_add=[('form_search', "Form Search"),
+                                                ('form_search_tree', "Form Search Tree")
+                                                ])
 
 
--- web_form_search/models/ir_ui_view.py
+++ web_form_search/models/ir_ui_view.py
@@ -5,4 +5,4 @@ from odoo import fields, models
 class View(models.Model):
     _inherit = 'ir.ui.view'
 
-    type = fields.Selection(selection_add=[('form_search', "Form Search")])
+    type = fields.Selection(selection_add=[('form_search', "Form Search"), ('form_search_tree', "Form Search Tree")])
