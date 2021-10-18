112b894d24b827af5cffac2d89e004c0c2585245
Osama Al Arbid <osama.al-arbid@arbetsformedlingen.se>
2021-10-18 16:54:07 +0200

    update manifests and remove commented lines

--- web_backend_theme_af/__manifest__.py
+++ web_backend_theme_af/__manifest__.py
@@ -15,6 +15,7 @@
 		v 12.0.1.1.2 Changed colors of navlinks\n
 		v 12.0.1.1.3 AFC-2836 changed the padding of label that ended up under buttons\n
 		v 12.0.1.1.4 AFC-2860 fixed overlapping problem with div af_apps_sidebar_panel\n  
+		v 12.0.1.1.5 AFC-2714 Split the text into a customer image for better readability\n  
     """,
     "author": "Vertel AB",
     "license": "LGPL-3",
