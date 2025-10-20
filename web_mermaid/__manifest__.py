2771488d5ef8ae01629ce6b26807d779b049050b
vertelbot <vertelbot@vertel.se>
2025-10-20 07:24:45 +0000

    odoobranchpfile odoo-web from 18.0. Do not p-file sync

---
+++ web_mermaid/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Web Mermaid",
+    "version": "18.0.1.0.1",
+    "license": "AGPL-3",
+    "sequence": 6,
+    "author": "Vertel AB",
+    "website": "https://www.vertel.se",
+    "summary": 'Base module to add mermaid in any model',
+    "description": """
+        Base module to add mermaid in any model""",
+    "depends": [
+        "web",
+        'web_widget_mermaid', # https://github.com/OCA/web/tree/14.0/web_widget_mermaid
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "application": True,
+}
