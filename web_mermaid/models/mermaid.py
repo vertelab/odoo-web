606e9459e04c44cacb0e7fc9cd6da20edf91e3e1
vertelbot <vertelbot@vertel.se>
2025-10-20 15:03:56 +0000

    odoobranchpfile odoo-web from 18.0. Do not p-file sync

--- web_mermaid/models/mermaid.py
+++ web_mermaid/models/mermaid.py
@@ -125,35 +125,5 @@ class MermaidMixin(models.AbstractModel):
             if wrapped_content != rec.mermaid_editor:
                 rec.mermaid_editor = wrapped_content
 
-    def _mermaid_prompt(self):
-        return f"""
-mermaid_type: {self.diagram_type}
-
-{self.prompt}
-        """
-
-    def _process_mermaid_prompt(self):
+    def action_build_mermaid(self):
         pass
-
-
-    def action_process_mermaid_syntax(self):
-        self.ensure_one()
-
-        if not self.prompt:
-            raise UserError(_("Please enter a prompt first."))
-
-        try:
-            quest_id = self.env['ai.quest'].browse(42).exists()
-            result = quest_id.run(prompt=self._mermaid_prompt(), record=self)
-
-            if result:
-                ai_messages = quest_id._get_last_ai_message(result.get('result', {}).get('messages', False))
-                print(ai_messages.content)
-                if not ai_messages:
-                    raise UserError(_("OBS: An error occurred, you should contact administrator to look into the quest"))
-
-                self.write({'mermaid_editor': ai_messages.content})
-                self.env.cr.commit()
-        except Exception as e:
-            _logger.warning(f"Error: {e}")
-
