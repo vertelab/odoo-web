0d0727cc86185836e387c9220f1a1666bd98a8f9
vertelbot <vertelbot@vertel.se>
2025-10-21 08:23:48 +0000

    odoobranchpfile odoo-web from 18.0. Do not p-file sync T/6282

--- web_mermaid/models/mermaid.p.py
+++ web_mermaid/models/mermaid.p.py
@@ -125,5 +125,24 @@ class MermaidMixin(models.AbstractModel):
             if wrapped_content != rec.mermaid_editor:
                 rec.mermaid_editor = wrapped_content
 
-    def action_build_mermaid(self):
-        pass
+    def _mermaid_prompt(self):
+        return f"""
+mermaid_type: {self.diagram_type}
+
+{self.prompt}
+        """
+
+    def _process_mermaid_prompt(self):
+        return False
+
+
+    def action_process_mermaid_syntax(self):
+        self.ensure_one()
+
+        if not self.prompt:
+            raise UserError(_("Please enter a prompt first."))
+
+        if mermaid_syntax := self._process_mermaid_prompt():
+            self.write({'mermaid_editor': f"<pre>{mermaid_syntax}</pre>"})
+
+
